eb28b0814484771b6ef7fa87be2fea4641a4c136
sibirser <sibirser@yandex.ru>
2015-12-18 20:14:27 +0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,107 +1,113 @@
 (function(){
 
-  var minus       = document.querySelectorAll(".range-control__minus");
-  var plus        = document.querySelectorAll(".range-control__plus");
-  var rangeInput  = document.querySelectorAll(".range-control__input");
+  var range = document.querySelectorAll(".range-control");
 
-  var parsedInput1 = parseInt(rangeInput[0].value, 10);
-  var parsedInput2 = parseInt(rangeInput[1].value, 10);
-  var output1 = rangeInput[0];
-  var output2 = rangeInput[1];
-
-  minus[0].addEventListener("click", function(event){
-    event.preventDefault();
-    if (parsedInput1 > 0) output1.value = parsedInput1-- + ' ' + 'дней';
-  });
-
-  plus[0].addEventListener("click", function(event){
-    event.preventDefault();
-    (parsedInput1 < 365)?   output1.value = parsedInput1++ + ' ' + 'дней' :
-                                      alert("Не более 365 дней");
-  });
-
-
-  minus[1].addEventListener("click", function(event){
-    event.preventDefault();
-    if (parsedInput2 > 0) output2.value = parsedInput2-- + ' ' + 'чел';
-  });
-
-  plus[1].addEventListener("click", function(event){
-    event.preventDefault();
-    (parsedInput2 < 50)?  output2.value = parsedInput2++ + ' ' + 'чел' :
-                          alert("Не более 50 человек");
-  });
-
-  output1.addEventListener("change", function(){
-    if (isNaN(output1.value)) alert("Введите число");
-  });
-})(),
-
-(function(){
-
-  if (!("FormData" in window)) {
-    return;
+  for (i=0; i < range.length; i++) {
+    numberField(range[i]);
   }
 
-  var form = document.querySelector(".member-form");
-  var btnClose = document.querySelectorAll(".photo__close");
+  function numberField(elements){
+    var input = elements.querySelector(".range-control__input");
+    var minus = elements.querySelector(".range-control__minus");
+    var plus  = elements.querySelector(".range-control__plus");
 
-  if ("FileReader" in window) {
-    form.querySelector("#photo-download").addEventListener("change", function(){
-
-      var files = this.files;
-
-      for(var i =0; i < files.length; i++) {
-        preview(files[i]);
-      }
-
+    plus.addEventListener("tap", function(event){
+      event.preventDefault();
+      calculate(true);
     });
 
-    function preview(file) {
-      var area = document.querySelector(".imgs__photo-wrap");
-      if (file.type.match(/image.*/)) {
-        var reader = new FileReader();
+    minus.addEventListener("tap", function(event){
+      event.preventDefault();
+      calculate(false);
+    });
 
-        reader.addEventListener("load", function(event){
-          var imgItem = document.createElement("div");
-          var figure = document.createElement("figure");
-          var img = document.createElement("img");
-          var figcapt = document.createElement("figcaption");
-          var btnPhoto = document.createElement("button");
+    function calculate(num){
+      var value = parseInt(input.value, 10);
 
-          imgItem.classList.add("imgs__photo-item");
-          figure.classList.add("photo");
-          img.src = event.target.result;
-          img.alt = file.name;
-          figcapt.classList.add("photo__title");
-          figcapt.innerHTML = file.name.slice(0, 5)+file.name.slice(-4);
-          btnPhoto.classList.add("photo__close");
+      if (isNaN(value)) {
+        value = 0;
+      }
 
-          area.appendChild(imgItem);
-          imgItem.appendChild(figure);
-          figure.appendChild(img);
-          figure.appendChild(figcapt);
-          imgItem.appendChild(btnPhoto);
-        });
-
-
-
-        reader.readAsDataURL(file);
+      if (num) {
+        input.value = (value < 365) ? (value + 1 + " " + "дней") : (value = 365 + " " + "дней");
+      }
+      else {
+        input.value = (value > 0) ? (value - 1 + " " + "дней") : (value = 0 + " " + "дней");
       }
     }
   }
 
-  // [].forEach.call(btnClose, function(close){
-  //   close.addEventListener("click", function(event){
-  //     event.preventDefault();
-  //     area.removeChild(imgItem);
-  //   });
-  // });
+})(),
+
+(function(){
+
+  if (!("FormData" in window) || !("FileReader" in window)) {
+    return;
+  }
+
+  var form = document.querySelector(".member-form");
+  var area = form.querySelector(".imgs__photo-wrap");
+  var queue = [];
+  var template = document.querySelector("#image-template").innerHTML;
+
+
+  form.querySelector("#photo-download").addEventListener("change", function(){
+
+    var files = this.files;
+
+    for (var i =0; i < files.length; i++) {
+      preview(files[i]);
+    }
+    this.value = "";
+
+  });
+
+  function preview(file) {
+    if (file.type.match(/image.*/)) {
+      var reader = new FileReader();
+
+      reader.addEventListener("load", function(event){
+        var html = Mustache.render(template, {
+                "image": event.target.result,
+                "name": file.name
+              });
+
+        var div = document.createElement("div");
+        div.classList.add("imgs__photo-item");
+        div.innerHTML = html;
+
+        area.appendChild(div);
+
+        div.querySelector(".photo__close").addEventListener("click", function(event){
+          event.preventDefault();
+          removePreview(div);
+        });
+
+        queue.push({file: file, div: div}); 
+      });
+
+
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function removePreview(div) {
+    queue = queue.filter(function(element){
+      return element.div != div;
+    });
+
+    div.parentNode.removeChild(div);
+  }
 
   form.addEventListener("submit", function(event){
     event.preventDefault();
 
     var data = new FormData(form);
+
+    queue.forEach(function(element){
+      data.append("images", element.file);
+    });
 
     request(data, function(response){
       console.log(response);
@@ -121,6 +127,5 @@
 
     xhr.send(data);
   }
-
 
 })();
